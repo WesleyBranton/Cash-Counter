c36c9a4bb5d8b51c3cf4ba17518b21c188407eb6
Wesley Branton <addons@wesleybranton.com>
2019-05-31 17:56:14 -0400

    Added license headers

--- firefox/popup/script.js
+++ firefox/popup/script.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 browser.storage.local.get('setting',startup);
 document.getElementsByTagName('table')[0].addEventListener('change',calculate);
 browser.storage.local.get("count", load);
