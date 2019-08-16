f2d54d3a0e6ef5bdea229e72420e000ed5db9acb
Wesley Branton <addons@wesleybranton.com>
2019-08-16 00:34:24 -0400

    Formatting change

--- firefox/background.js
+++ firefox/background.js
@@ -3,35 +3,36 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 function handleInstalled(details) {
-	// Creates Setting Defaults After Installation
-	if (details.reason == 'install') {
-		browser.storage.local.set({
-			setting: {
-				displayroll: true,
-				set12coin: false
-			},
-			roll: {
-				d00001: 50*0.01,
-				d00005: 40*0.05,
-				d00010: 50*0.1,
-				d00025: 40*0.25,
-				d00100: 25*1,
-				d00200: 25*2
-			},
-			count: {
-				d00001: 0,
-				d00005: 0,
-				d00010: 0,
-				d00025: 0,
-				d00100: 0,
-				d00200: 0,
-				d00500: 0,
-				d01000: 0,
-				d02000: 0,
-				d05000: 0,
-				d10000: 0
-			}
-		});
-	}
+    // Creates setting defaults after installation
+    if (details.reason == 'install') {
+        browser.storage.local.set({
+            setting: {
+                displayroll: true,
+                set12coin: false
+            },
+            roll: {
+                d00001: 50 * 0.01,
+                d00005: 40 * 0.05,
+                d00010: 50 * 0.1,
+                d00025: 40 * 0.25,
+                d00100: 25 * 1,
+                d00200: 25 * 2
+            },
+            count: {
+                d00001: 0,
+                d00005: 0,
+                d00010: 0,
+                d00025: 0,
+                d00100: 0,
+                d00200: 0,
+                d00500: 0,
+                d01000: 0,
+                d02000: 0,
+                d05000: 0,
+                d10000: 0
+            }
+        });
+    }
 }
+
 browser.runtime.onInstalled.addListener(handleInstalled);
